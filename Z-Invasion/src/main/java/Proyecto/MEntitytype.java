cb48480e04a9bf4920def384f8da02586640fcf9
Joel Rosales <91057061+JoelPickTheWorngHouse@users.noreply.github.com>
2022-02-11 07:02:30 +0100

    Add files via upload

---
+++ Z-Invasion/src/main/java/Proyecto/MEntitytype.java
@@ -0,0 +1,18 @@
+package app;
+
+public enum MEntitytype {
+	personaje,
+	puntoInicio,
+	mensaje1,
+	plataforma,
+	mensaje2,
+	muerte,
+	mensaje3,
+	enemigoNormal,
+	mensaje4,
+	mensajes5,
+	enemigoDisparo,
+	salida,
+	moneda,
+	BALA
+}
