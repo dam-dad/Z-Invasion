eebff3adb0ed4f629ff43eecce2617892ac8e146
Joel Rosales <91057061+JoelPickTheWorngHouse@users.noreply.github.com>
2022-02-11 06:11:54 +0000

    Delete Z-Invasion/resources/assets directory

--- Z-Invasion/resources/assets/levels.tmx/tiles_spritesheet.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.0" name="tiles_spritesheet" tilewidth="70" tileheight="70" spacing="2" tilecount="156" columns="12">
- <image source="tiles_spritesheet.png" width="914" height="936"/>
-</tileset>
