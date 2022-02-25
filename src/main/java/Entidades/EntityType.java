811ad47380a3ff2efb10a7c81ef09cbef3839835
Joel Rosales <91057061+JoelPickTheWorngHouse@users.noreply.github.com>
2022-02-25 06:34:00 +0000

    Update EntityType.java

--- src/main/java/Entidades/EntityType.java
+++ src/main/java/Entidades/EntityType.java
@@ -1,5 +1,8 @@
 package Entidades;
 
+/**
+ *Entidades con las que damos propiedades a los objetos de los mapas
+ */
 public enum EntityType {
 	PLAYER, MONEDA, PLATAFORMA, PUERTA, BALA, ENEMY,reaparecer, mensaje,muerte
 }
